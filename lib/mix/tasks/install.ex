0009df2b6db72a1275fa3835c63bf9e72a765218
thehaigo <sirannha@gmail.com>
2024-03-13 03:02:30 +0900

    add db setting

--- lib/mix/tasks/install.ex
+++ lib/mix/tasks/install.ex
@@ -7,24 +7,22 @@ defmodule Mix.Tasks.Desktop.Install do
     {parsed_args, _, _} =
       OptionParser.parse(args, strict: [namespace: :string, os: :string, database: :string])
 
-    # Get all Mix tasks for Elixir Desktop client project
-    # valid_mix_tasks = get_installer_mix_tasks()
     host_project_config = Util.get_host_project_config(parsed_args)
-    # run_all_install_tasks(valid_mix_tasks, host_project_config)
+    database = get_database(host_project_config) |> IO.inspect()
 
-    # IO.inspect(host_project_config)
     update_config_exs_if_needed(host_project_config)
     update_endpoint_ex_if_needed(host_project_config)
-    update_application_ex_if_needed(host_project_config)
+    update_application_ex_if_needed(host_project_config, database)
     update_mix_exs_application_if_needed(host_project_config)
-    update_mix_exs_deps_if_needed(host_project_config)
+    update_mix_exs_deps_if_needed(host_project_config, database)
+    update_repo_ex_if_needed(host_project_config, database)
+    update_prod_ex_if_needed(host_project_config, database)
     rename_runtime_exs_if_needed(host_project_config)
     clean_build_path(host_project_config)
     format_config_files()
 
     IO.puts("\nYour Phoenix app is ready to use ElixirDesktop!\n")
     IO.puts("\nStart your Desktop app with: \n \n $ iex -S mix\n")
-    # IO.puts("Platform-specific project files have been placed in the \"native\" directory\n")
     :ok
   end
 
@@ -104,7 +102,22 @@ defmodule Mix.Tasks.Desktop.Install do
     end
   end
 
-  defp update_mix_exs_deps_if_needed(%{mix_config_path: path}) do
+  defp get_database(%{mix_config_path: path}) do
+    {:ok, config_body} = File.read(path)
+
+    cond do
+      String.contains?(config_body, ":ecto_sqlite3") ->
+        :sqlite
+
+      String.contains?(config_body, ":postgrex") ->
+        :postgresql
+
+      true ->
+        :no_ecto
+    end
+  end
+
+  defp update_mix_exs_deps_if_needed(%{mix_config_path: path}, database) do
     replace_string = "{:desktop, \"~> 1.5\"}"
 
     {:ok, config_body} = File.read(path)
@@ -120,7 +133,7 @@ defmodule Mix.Tasks.Desktop.Install do
         config_body
         |> String.replace(
           ~r/defp deps do\n    \[\n/,
-          "defp deps do\n    \[\n       {:desktop, \"~> 1.5\"}, \n{:wx, \"~> 1.1\", hex: :bridge, targets: [:android, :ios]},\n"
+          "defp deps do\n    \[\n       {:desktop, \"~> 1.5\"}, \n{:wx, \"~> 1.1\", hex: :bridge, targets: [:android, :ios]},\n#{if database == :sqlite, do: "{:exqlite, github: \"elixir-desktop/exqlite\", override: true},\n", else: ""}"
         )
 
       IO.binwrite(app_config, updated_config_body)
@@ -129,12 +142,15 @@ defmodule Mix.Tasks.Desktop.Install do
     end
   end
 
-  defp update_application_ex_if_needed(%{
-         application_path: path,
-         app_name: app_name,
-         app_namespace: app_namespace,
-         app_config_path: config_path
-       }) do
+  defp update_application_ex_if_needed(
+         %{
+           application_path: path,
+           app_name: app_name,
+           app_namespace: app_namespace,
+           app_config_path: config_path
+         },
+         database
+       ) do
     replace_string = "Desktop.OS.home()"
 
     {:ok, config_body} = File.read(path)
@@ -146,14 +162,14 @@ defmodule Mix.Tasks.Desktop.Install do
 
       {:ok, app_config} = File.open(path, [:write])
 
-      updated_config_body = application_body(app_name, app_namespace, config_path)
+      updated_config_body = application_body(app_name, app_namespace, config_path, database)
 
       IO.binwrite(app_config, updated_config_body)
       File.close(app_config)
     end
   end
 
-  defp application_body(app_name, app_namespace, config_path) do
+  defp application_body(app_name, app_namespace, config_path, database) do
     adapter = adapter_setting(app_namespace, config_path)
     repo = repo_setting(app_name, app_namespace)
 
@@ -169,6 +185,9 @@ defmodule Mix.Tasks.Desktop.Install do
       def start(:normal, []) do
         File.mkdir_p!(config_dir())
 
+        #{if database == :sqlite, do: "Application.put_env(:#{app_name}, #{app_namespace}.Repo,
+        database: Path.join(config_dir(), \"/database.sq3\"))"}
+
         :session = :ets.new(:session, [:named_table, :public, read_concurrency: true])
 
         children = [
@@ -179,6 +198,8 @@ defmodule Mix.Tasks.Desktop.Install do
 
         opts = [strategy: :one_for_one, name: #{app_namespace}.Supervisor]
         {:ok, sup} = Supervisor.start_link(children, opts)
+
+        #{if database == :sqlite, do: "#{app_namespace}.Repo.migration()"}
 
         #{adapter}
 
@@ -221,6 +242,79 @@ defmodule Mix.Tasks.Desktop.Install do
     else
       "{:ok, {_ip, port}} = Bandit.PhoenixAdapter.server_info(#{app_namespace}Web.Endpoint, :http)"
     end
+  end
+
+  defp update_repo_ex_if_needed(%{repo_path: path}, :sqlite) do
+    replace_string = "migration"
+    {:ok, repo_body} = File.read(path)
+
+    if String.contains?(repo_body, replace_string) do
+      IO.puts("repo.ex deps already modified, skipping...")
+    else
+      Owl.IO.puts([Owl.Data.tag("* updating ", :yellow), "repo.ex"])
+
+      {:ok, repo_config} = File.open(path, [:write])
+
+      updated_config_body =
+        repo_body
+        |> String.replace(
+          ~r/adapter: Ecto.Adapters.SQLite3/,
+          "adapter: Ecto.Adapters.SQLite3 \n\n def migration() do\nend"
+        )
+
+      IO.binwrite(repo_config, updated_config_body)
+      File.close(repo_config)
+    end
+  end
+
+  defp update_repo_ex_if_needed(_, _), do: :ok
+
+  defp update_prod_ex_if_needed(
+         %{prod_path: path, app_name: app_name, app_namespace: app_namespace},
+         :postgresql
+       ) do
+    replace_string = "#{app_namespace}.Repo"
+    {:ok, repo_body} = File.read(path)
+
+    if String.contains?(repo_body, replace_string) do
+      IO.puts("prod.exs deps already modified, skipping...")
+    else
+      Owl.IO.puts([Owl.Data.tag("* updating ", :yellow), "prod.ex"])
+
+      {:ok, repo_config} = File.open(path, [:write])
+
+      updated_config_body =
+        repo_body
+        |> String.replace(
+          ~r/import Config/,
+          """
+          import Config
+
+          config :#{app_name}, #{app_namespace}.Repo,
+          username: "postgres",
+          password: "postgres",
+          database: "#{app_name}_dev",
+          stacktrace: true,
+          show_sensitive_data_on_connection_error: true,
+          pool_size: 10
+
+          if Mix.target() == :ios do
+          config :#{app_name}, #{app_namespace}.Repo, hostname: "127.0.0.1"
+          end
+
+          if Mix.target() == :android do
+          config :#{app_name}, #{app_namespace}.Repo, hostname: "10.0.2.2"
+          end
+          """
+        )
+
+      IO.binwrite(repo_config, updated_config_body)
+      File.close(repo_config)
+    end
+  end
+
+  defp update_prod_ex_if_needed(_, _) do
+    IO.puts("prod.exs no use postgrex, skipping...")
   end
 
   def rename_runtime_exs_if_needed(%{runtime_path: path}) do
